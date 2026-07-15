4951451c5a63209e0cfe6cc1d6e9c4fc8878fa1e
redgirl <redgirlll@gmail.com>
2026-07-15 14:55:52 +0300

    intial commit

---
+++ pro1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a=20;
+	int b=10;
+int sum;
+int sub;
+int divide;
+int multiply;
+       sum=a+b;
+	   sub=a-b;
+	   divide=a%b;
+	   multiply=a*b;
+	cout<<"display the sum of the number"<<" "<<sum<<endl;
+	cout<<"display the sub of the number"<<" "<<sub<<endl;
+	cout<<"display the divide of the number"<<" "<<divide<<endl;
+	cout<<"display the multiply of the number"<<" "<<multiply<<endl;
+	cout<<++a<<endl;
+	cout<<(a==b)<<endl;	
+}
